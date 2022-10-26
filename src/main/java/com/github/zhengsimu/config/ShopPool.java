0ad9ac7ee1d9f6911017ad3fd49f0d9d503f26a7
simu <1130377654@qq.com>
2022-10-26 11:32:04 +0800

    add SignerInterceptor

--- src/main/java/com/github/zhengsimu/config/ShopPool.java
+++ src/main/java/com/github/zhengsimu/config/ShopPool.java
@@ -7,5 +7,15 @@ import com.github.zhengsimu.ShopTokenInfo;
  */
 public interface ShopPool {
 
-    ShopTokenInfo getShop();
+    /**
+     * 简要说明:
+     * 通过卖家ID获得店铺令牌信息
+     * Get the store token information through the seller ID
+     *
+     * @param sellerId 卖家ID
+     * @return 店铺令牌信息
+     * @author: zhengsimu
+     * 创建时间:2022/10/25 16:52
+     */
+    ShopTokenInfo getShop(String sellerId);
 }
--- src/main/java/com/github/zhengsimu/signer/SignerInterceptor.java
+++ src/main/java/com/github/zhengsimu/signer/SignerInterceptor.java
@@ -1,9 +1,15 @@
 package com.github.zhengsimu.signer;
 
+import com.amazonaws.util.StringUtils;
+import com.github.zhengsimu.Marketplace;
+import com.github.zhengsimu.ShopTokenInfo;
+import com.github.zhengsimu.config.ShopPool;
 import okhttp3.Interceptor;
+import okhttp3.Request;
 import okhttp3.Response;
 
 import java.io.IOException;
+import java.util.Locale;
 
 /**
  * 类名:com.github.zhengsimu.signer
@@ -15,8 +21,34 @@ import java.io.IOException;
  */
 public class SignerInterceptor implements Interceptor {
 
+    private final ShopPool shopPool;
+
+    public SignerInterceptor(ShopPool shopPool) {
+        this.shopPool = shopPool;
+    }
+
     @Override
     public Response intercept(Chain chain) throws IOException {
-        return null;
+        Request request = chain.request();
+        String sellerId = request.header("sellerId");
+        if (!StringUtils.isNullOrEmpty(sellerId)){
+            String marketplaceId = request.header("marketplaceId");
+            ShopTokenInfo shop  = this.shopPool.getShop(sellerId);
+            Request.Builder builder = request.newBuilder().removeHeader("sellerId");
+            assert marketplaceId != null;
+            Marketplace marketplace = Marketplace.valueOf(marketplaceId.toUpperCase(Locale.ROOT));
+            builder.url(request.url().newBuilder().host(marketplace.getEndpoint().getEndpoint()).build())
+                    .addHeader("x-amz-access-token", shop.getAccessToken())
+                    .addHeader("Content-Type", "application/json;charset=UTF-8");
+            AWSSigV4Signer signer = new AWSSigV4Signer(AwsAuthenticationCredentials.builder()
+                    .accessKeyId(shop.getCredentials().getAccessKeyId())
+                    .secretKey(shop.getCredentials().getSecretKey())
+                    .region(marketplace.getEndpoint().getRegion())
+                    .roleArn(shop.getCredentials().getRoleArn())
+                    .roleSessionName(shop.getCredentials().getRoleSessionName())
+                    .build());
+            request = signer.sign(builder.build());
+        }
+        return chain.proceed(request);
     }
 }
